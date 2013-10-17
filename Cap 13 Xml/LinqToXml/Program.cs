2cb023e629a7c8331593fb08196ebb4aebba8f9b
Antonio Pelleriti <antonio.pelleriti@dynamicode.it>
2013-10-17 18:21:59 +0200

    esempio linq to xml

--- Cap 13 Xml/LinqToXml/Program.cs
+++ Cap 13 Xml/LinqToXml/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +27,50 @@ namespace LinqToXml
             xdoc.Save("xfile.xml");
             //Debug.WriteLine(xdoc);
             Console.WriteLine(xdoc);
+
+            var veicoli=GetVeicoli();
+            var xmlVeicoli = new XElement("veicoli", from v in veicoli
+                                                     select new XElement("veicolo", new XAttribute("targa", v.Targa),
+                                                                   new XElement("marca", v.Marca),
+                                                                   new XElement("modello", v.Modello),
+                                                                   new XElement("alimentazione", new XAttribute("tipo",v.Alimentazione),
+                                                                       new XElement("consumo",v.Consumo)
+                                                                   )
+                                                             )
+                            );
+
+            Console.WriteLine(xmlVeicoli);
+
+            string filename = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "myfile.xml");
+            
+            var xd= XElement.Load(filename);
+            var query = from veicolo in xd.Descendants("veicolo")
+                        select veicolo.Attribute("targa").Value;
+
+            query.ToList<string>().ForEach(Console.WriteLine);
+
+
+            Console.ReadLine();
         }
+
+        public static List<Veicolo> GetVeicoli()
+        {
+            List<Veicolo> lista = new List<Veicolo>();
+            lista.Add(new Veicolo() { Marca = "Alfa Romeo", Modello = "GT", Targa = "AB123CD", Alimentazione = "Diesel", Consumo = 14.5 });
+            lista.Add(new Veicolo() { Marca = "FIAT", Modello = "Uno", Targa = "ME4563433", Alimentazione = "Benzina", Consumo=5 });
+
+            return lista;
+        }
+    }
+
+
+
+    public class Veicolo
+    {
+        public String Targa { get; set; }
+        public string Modello { get; set; }
+        public string Marca { get; set; }
+        public string Alimentazione { get; set; }
+        public double Consumo { get; set; }
     }
 }
