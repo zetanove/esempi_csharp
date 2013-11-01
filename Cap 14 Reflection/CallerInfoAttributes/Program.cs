d8cd0e54b6436004e6cabec3414eade54d6704c7
zetanove <antonio.pelleriti@dynamicode.it>
2013-11-01 01:07:39 +0100

    esempi caller info

---
+++ Cap 14 Reflection/CallerInfoAttributes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CallerInfoAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogInfo(); //riga 10
+
+            PropertyInt = 1;
+        }
+
+        public static int PropertyInt
+        {
+            get
+            {
+                LogInfo();
+                return 0;
+            }
+            set
+            {
+                LogInfo();
+            }
+        }
+
+        public static void LogInfo(
+                [CallerMemberName] string memberName = null,
+                [CallerFilePath] string filePath = null,
+                [CallerLineNumber] int lineNumber = 0)
+        {
+            Console.WriteLine(memberName);
+            Console.WriteLine(filePath);
+            Console.WriteLine(lineNumber);
+        }
+    }
+}
