225f1c719d5daa38d6d069bdf05a10bc9da0a8ed
Antonio Pelleriti <antonio.pelleriti@dynamicode.it>
2013-11-07 09:54:07 +0100

    attributes

---
+++ Cap 14 Reflection/CustomAttributes/VehicleTypeAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttributes
+{
+    class VehicleTypeAttribute : Attribute
+    {
+        public string TypeDescription { get; set; }
+
+        public VehicleTypeAttribute(string desc)
+        {
+            TypeDescription = desc;
+        }
+    }
+
+
+    [VehicleType("Automobile")]
+    class Car
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class MyAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string positionalString;
+
+        // This is a positional argument
+        public MyAttribute(string positionalString)
+        {
+            this.positionalString = positionalString;
+
+            // TODO: Implement code here
+            throw new NotImplementedException();
+        }
+
+        public string PositionalString
+        {
+            get { return positionalString; }
+        }
+
+        // This is a named argument
+        public int NamedInt { get; set; }
+    }
+}
