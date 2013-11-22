de288606a538563490c1c54fdca8e4d2900eedcd
zetanove <antonio.pelleriti@dynamicode.it>
2013-11-22 09:36:25 +0100

    execute reader

--- Cap 15 Accesso ai dati/ADONet/Program.cs
+++ Cap 15 Accesso ai dati/ADONet/Program.cs
@@ -25,7 +25,21 @@ namespace ADONet
 
                 string sql = "SELECT * FROM SalesLT.Customer";
                 SqlCommand cmd = new SqlCommand(sql, connection);
-               
+
+                SqlDataReader reader= cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string name = reader["FirstName"] as string;
+                        string lastname = reader["LastName"] as string;
+                        Console.WriteLine("{0} {1} {2}", id, name, lastname);
+                    }
+                }
+                reader.Close();
+                
             }
         }
     }
