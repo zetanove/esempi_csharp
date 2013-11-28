45c712ec19729ce44e66700c68f58e66e0648396
zetanove <antonio.pelleriti@dynamicode.it>
2013-11-28 14:11:24 +0100

    modello EF adventure works

---
+++ Cap 15 Accesso ai dati/LinqToSql/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSql
+{
+    [Table(Name="SalesLT.Product")]
+    class MyProduct
+    {
+        [Column(IsPrimaryKey=true)]
+        public int ProductID { get; set; }
+        [Column]
+        public string Name { get; set; }
+        [Column]
+        public string Color { get; set; }
+    }
+}
---
+++ Cap 15 Accesso ai dati/LinqToSql/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string connString = "Server=(LocalDb)\\v11.0;Integrated Security=true;database=AdventureWorksLT2012";
+            DataContext context = new DataContext(connString);
+
+            var tableProd = context.GetTable<MyProduct>();
+            var products=from prod in tableProd
+                         where prod.Color=="Black"
+                         select prod;
+
+            foreach (MyProduct p in products)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            MyProduct product = products.First();
+            product.Color = "Red";
+            context.SubmitChanges();
+
+            AdventureWorksDataContext awdc = new AdventureWorksDataContext(connString);
+            var categories= awdc.ProductCategories;
+
+            foreach(ProductCategory pc in categories)
+            {
+                Console.WriteLine(pc.Name);
+                foreach(Product pd in pc.Products)
+                    Console.WriteLine("   {0}", pd.Name);
+            }
+
+            var query = from cat in categories
+                        from prod in cat.Products
+                        where cat.Name.Contains("Frames")
+                        select prod;
+
+            foreach(var p in query)
+            {
+                Console.WriteLine("{0}", p.Name);
+            }
+
+            awdc.Products.First().Color = "Dark Red";
+            awdc.SubmitChanges();
+
+            ProductCategory newCat = new ProductCategory();
+            newCat.Name = "Nuova Categoria";
+            newCat.ModifiedDate = DateTime.Now;
+            awdc.ProductCategories.InsertOnSubmit(newCat);
+            awdc.SubmitChanges();
+        }
+    }
+}
