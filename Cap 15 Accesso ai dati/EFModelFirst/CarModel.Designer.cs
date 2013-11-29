0c3d395ed3e9fe5227c3c319a775cacbe67725d4
Antonio Pelleriti <antonio.pelleriti@dynamicode.it>
2013-11-29 19:03:24 +0100

    ef examples

--- Cap 15 Accesso ai dati/EFModelFirst/CarModel.Designer.cs
+++ Cap 15 Accesso ai dati/EFModelFirst/CarModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\dev\esempi_csharp\Cap 15 Accesso ai dati\EntityFramework\AdventureWorksModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\dev\esempi_csharp\Cap 15 Accesso ai dati\EFModelFirst\CarModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Cap 15 Accesso ai dati/EntityFramework/Program.cs
+++ Cap 15 Accesso ai dati/EntityFramework/Program.cs
@@ -38,8 +38,28 @@ namespace EntityFramework
                 cat1.rowguid = Guid.NewGuid();
                 using (AdventureWorksLT2012Entities dba = new AdventureWorksLT2012Entities())
                 {
+                    dba.Database.ExecuteSqlCommand("DELETE from SalesLT.ProductCategory WHERE Name='nuova cat'");
                     dba.ProductCategory.Add(cat1);
                     dba.SaveChanges();
+                }
+
+                using(AdventureWorksLT2012Entities dbquery = new AdventureWorksLT2012Entities())
+                {
+                    var product1 = dbquery.Product.Find(1);
+
+                    var products = dbquery.Product.SqlQuery("select * from SalesLT.Product").ToList();
+
+                    var productColorsQuery = dbquery.Database.SqlQuery<string>("select distinct Color from SalesLT.product");
+                    List<string> names= productColorsQuery.ToList();
+
+
+                    int affected=dbquery.Database.ExecuteSqlCommand("UPDATE SalesLT.Customer SET CompanyName='ACME' WHERE CustomerID=1234");
+                }
+
+                //loading
+                using (AdventureWorksLT2012Entities dbload = new AdventureWorksLT2012Entities())
+                {
+                    var productOrdered= dbload.Customer.Where(c => c.SalesOrderHeader.Any()).First().SalesOrderHeader.First().SalesOrderDetail.First().Product;
                 }
             }
         }
