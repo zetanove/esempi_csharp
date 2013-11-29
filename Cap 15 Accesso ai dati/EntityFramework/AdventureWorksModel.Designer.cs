4d8a58250dfec1d07678f8fd9122f68a001c4f0f
zetanove <antonio.pelleriti@dynamicode.it>
2013-11-29 11:08:59 +0100

    ef database first

--- Cap 15 Accesso ai dati/EntityFramework/AdventureWorksModel.Designer.cs
+++ Cap 15 Accesso ai dati/EntityFramework/AdventureWorksModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Dev\esempi_csharp\Cap 15 Accesso ai dati\EntityFramework\AdventureWorks.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Dev\esempi_csharp\Cap 15 Accesso ai dati\EntityFramework\AdventureWorksModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Cap 15 Accesso ai dati/EntityFramework/Program.cs
+++ Cap 15 Accesso ai dati/EntityFramework/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,36 @@ namespace EntityFramework
     {
         static void Main(string[] args)
         {
+            using (AdventureWorksLT2012Entities db = new AdventureWorksLT2012Entities())
+            {
+                var query = from prod in db.Product
+                            select prod;
+
+                foreach (Product p in query)
+                {
+                    Console.WriteLine("{0} {1}", p.ProductID, p.Name);
+                }
+
+                var q2 = from cat in db.ProductCategory
+                         join prod in db.Product on cat.ProductCategoryID equals prod.ProductCategoryID
+                         where cat.Name.ToLower().Contains("bike")
+                         select prod;
+
+                foreach(var p in q2)
+                {
+                    Console.WriteLine("{0} {1}", p.ProductCategory.Name, p.Name);
+                }
+
+                ProductCategory cat1 = new ProductCategory();
+                cat1.Name = "nuova cat";
+                cat1.ModifiedDate = DateTime.Now;
+                cat1.rowguid = Guid.NewGuid();
+                using (AdventureWorksLT2012Entities dba = new AdventureWorksLT2012Entities())
+                {
+                    dba.ProductCategory.Add(cat1);
+                    dba.SaveChanges();
+                }
+            }
         }
     }
 }
