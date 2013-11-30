92331fb341c11c233b570a3cbfa2beb03fed752b
zetanove <antonio.pelleriti@dynamicode.it>
2013-11-30 20:33:28 +0100

    esempi ef code first

---
+++ Cap 15 Accesso ai dati/EFCodeFirstApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using EFCodeFirstLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CarContext db = new CarContext())
+            {
+                Console.WriteLine("connected on {0}", db.Database.Connection.ConnectionString);
+                Car c = new Car();
+                c.Targa = "abc";
+                c.Modello = "Alfa Romeo GT";
+
+                Person p = new Person();
+                p.FirstName = "Antonio";
+                p.LastName = "Pelleriti";
+                c.Person = p;
+
+                db.Cars.Add(c);
+                db.People.Add(p);
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Cap 15 Accesso ai dati/EFCodeFirstLibrary/CarContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace EFCodeFirstLibrary
+{
+    public class CarContext:DbContext
+    {
+        public DbSet<Car> Cars {get;set;}
+        public DbSet<Person> People { get; set; }
+    }
+}
