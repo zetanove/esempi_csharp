43e5e54ecb5bfb5b7e0f486f0ac40f9c1cf7d024
zetanove <antonio.pelleriti@dynamicode.it>
2013-12-05 11:56:08 +0100

    cap 16 winforms

---
+++ Cap 16 Applicazioni CSharp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Hello Windows";
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hai cliccato il pulsante");
+        }
+    }
+}
