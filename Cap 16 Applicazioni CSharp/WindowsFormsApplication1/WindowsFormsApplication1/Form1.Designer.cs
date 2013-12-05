3b17f008937fc74455d297389da765dc9bee0ded
Antonio Pelleriti <antonio.pelleriti@dynamicode.it>
2013-12-05 18:13:49 +0100

    esempio winforms inizio wpf

--- Cap 16 Applicazioni CSharp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ Cap 16 Applicazioni CSharp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,33 +29,93 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.btShowChild = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(103, 95);
+            this.button1.Location = new System.Drawing.Point(24, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "cliccami";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btShowChild
+            // 
+            this.btShowChild.Location = new System.Drawing.Point(24, 73);
+            this.btShowChild.Name = "btShowChild";
+            this.btShowChild.Size = new System.Drawing.Size(75, 23);
+            this.btShowChild.TabIndex = 1;
+            this.btShowChild.Text = "Show Child";
+            this.btShowChild.UseVisualStyleBackColor = true;
+            this.btShowChild.Click += new System.EventHandler(this.btShowChild_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "scegli";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Open File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btShowChild);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btShowChild;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
 
     }
 }
--- Cap 16 Applicazioni CSharp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Cap 16 Applicazioni CSharp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -23,5 +23,33 @@ namespace WindowsFormsApplication1
         {
             MessageBox.Show("Hai cliccato il pulsante");
         }
+
+        private void btShowChild_Click(object sender, EventArgs e)
+        {
+            Form f2 = new Form();
+            f2.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("continuare?", "scegli", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if(result== System.Windows.Forms.DialogResult.OK)
+            {
+                label1.Text = "Hai cliccato OK";
+            }
+            else label1.Text = "Hai cliccato annulla";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "File di test (*.txt)|*.txt|Tutti i file (*.*)|*.*";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if(ofd.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                string filename = ofd.FileName;
+                MessageBox.Show("Hai selezionato il file "+filename);
+            }
+        }
     }
 }
