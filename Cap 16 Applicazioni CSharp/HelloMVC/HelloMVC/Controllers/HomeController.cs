ecc83c36d2a3a6a167545cbe6459b9e4693ba36e
Antonio Pelleriti <antonio.pelleriti@dynamicode.it>
2013-12-11 15:17:44 +0100

    esempi aspnet mvc

---
+++ Cap 16 Applicazioni CSharp/HelloMVC/HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            ViewBag.Hello = "Hello MVC from Controller";
+            return View();
+        }
+	}
+}
