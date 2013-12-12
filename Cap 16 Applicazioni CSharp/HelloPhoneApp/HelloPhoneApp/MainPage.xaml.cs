5cb720fb6900d1dcb2cb62075ea2cdc41eb36ba8
zetanove <antonio.pelleriti@dynamicode.it>
2013-12-12 17:58:43 +0100

    esempio windows phone

---
+++ Cap 16 Applicazioni CSharp/HelloPhoneApp/HelloPhoneApp/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using HelloPhoneApp.Resources;
+
+namespace HelloPhoneApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void Button_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            MessageBox.Show("Hello " + name);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string name=txtName.Text;
+            NavigationService.Navigate(new Uri("/Page2.xaml?parameter=" + name, UriKind.RelativeOrAbsolute));
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ Cap 16 Applicazioni CSharp/HelloPhoneApp/HelloPhoneApp/Page2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace HelloPhoneApp
+{
+    public partial class Page2 : PhoneApplicationPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            tb.Text="Hello "+ NavigationContext.QueryString["parameter"];
+        }
+    }
+}
